3db8b286d9040493a81a19c91922987ae0181ed6
Richard Pike <richard@richardpike.net>
2014-07-12 15:29:43 +0100

    Model for list items and assoc. migration

--- lists/tests.py
+++ lists/tests.py
@@ -5,6 +5,7 @@ from django.http import HttpRequest
 
 
 from lists.views import home_page
+from lists.models import Item
 
 
 class HomePageTest(TestCase):
@@ -26,7 +27,7 @@ class HomePageTest(TestCase):
 	def test_home_page_can_save_a_post_request(self):
 		request = HttpRequest()
 		request.method = 'POST'
-		request.POST['item_text'] = 'A new list item'
+		request.POST['item_name'] = 'A new list item'
 
 		response = home_page(request)
 
@@ -36,3 +37,23 @@ class HomePageTest(TestCase):
 			{'new_item_text': 'A new list item'}
 		)
 		self.assertEqual(response.content.decode(), expected_html)
+
+class ItemModelTest(TestCase):
+	def test_saving_and_returning_items(self):
+		first_item = Item()
+		first_item.text = 'The first (ever) list item'
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'Item the second'
+		second_item.save()
+
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(), 2)
+
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(second_saved_item.text, 'Item the second')
+
+
